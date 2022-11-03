2667cb4700a71310f811c98eb0460d8647647945
kxxt <rsworktech@outlook.com>
2022-11-03 16:20:15 +0800

    android cam example

---
+++ android-cam/cam.py
@@ -0,0 +1,16 @@
+
+import cv2 as cv
+
+ip = input("IP address of your phone: ")
+vid = cv.VideoCapture(f"http://{ip}:4747/video?640x480")
+
+while(True):
+    ret, frame = vid.read()
+    if not ret:
+        raise "Failed to read image!"
+    cv.imshow('image', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vid.release()
+cv.destroyAllWindows()
