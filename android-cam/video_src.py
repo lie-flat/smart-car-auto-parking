017866795fa94b903dc6f255967c3677ab81f0ff
kxxt <rsworktech@outlook.com>
2022-11-26 22:55:01 +0800

    fix: video src

--- android-cam/video_src.py
+++ android-cam/video_src.py
@@ -1,10 +1,10 @@
 import cv2 as cv
 from config import IS_RASPBERRYPI, ip
 
-if ip is None:
-    ip = input("IP address of your phone: ")
 
 if IS_RASPBERRYPI:
     vid = cv.VideoCapture(0)
 else:
+    if ip is None:
+        ip = input("IP address of your phone: ")
     vid = cv.VideoCapture(f"http://{ip}:4747/video?640x480")
