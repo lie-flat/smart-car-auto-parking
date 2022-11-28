082dc2a4fd7724268b9e52cb4b7825a540ac3b53
kxxt <rsworktech@outlook.com>
2022-11-28 16:28:16 +0800

    fix joystick

--- client/run/joystick.py
+++ client/run/joystick.py
@@ -1,52 +1,68 @@
-from pyjoystick.pygame import Key, Joystick, run_event_loop
-from ..controller import connect_to_board, read_sensors, control
 import logging
-import time
+import pygame
+from ..controller import connect_to_board, read_sensors, control
+from functools import partial
+
+pygame.init()
+clock = pygame.time.Clock()
+keepPlaying = True
 
 servo = 7.5
-motorA = 0
+motorA = 36
 motorB = 0
-lastControlTime = time.time()
 
 
-def print_add(joy):
-    print('Joystick connected')
+joysticks = []
+
+for i in range(0, pygame.joystick.get_count()):
+    # create an Joystick object in our list
+    joysticks.append(pygame.joystick.Joystick(i))
+    # initialize them all (-1 means loop forever)
+    joysticks[-1].init()
 
 
-def print_remove(joy):
-    print('Joystick removed', joy)
+def clamp(n, smallest, largest):
+    return max(smallest, min(n, largest))
 
 
-def key_received(key):
-    global servo, motorA, motorB, lastControlTime
-    log.info(
-        f"{key.number}:{key.value} Send servo={servo},motor={motorA},{motorB}")
-    if key.keytype == Key.AXIS:
-        if key.number == 0:
-            servo = key.value * 5 + 7.5
-        elif key.number == 1:
-            if key.value >= 0:
-                motorA = 0
-                motorB = key.value * 70
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s [%(name)s]%(levelname)s:%(message)s')
+log = logging.getLogger("main")
+devices = connect_to_board()
+log.info(f"Found devices: {devices}")
+ip = devices['board']
+control = partial(control, ip)
+# control = lambda *a, **b: None
+control(servo=7.5, motorA=0, motorB=0)
+
+lt = -1
+rt = -1
+speed = 0
+
+
+while keepPlaying:
+    clock.tick(20)
+    for event in pygame.event.get():
+        print(event)
+
+        if event.type == pygame.JOYAXISMOTION:
+            if event.axis == 2:
+                lt = event.value
+            elif event.axis == 5:
+                rt = event.value
             else:
-                motorA = key.value * 70
-                motorB = 0
-        t = time.time()
-        if t - lastControlTime > 0.1:
-            lastControlTime = t
-            log.info(
-                f"{key.number}:{key.value} Send servo={servo},motor={motorA},{motorB}")
-            control(boardIP, servo, motorA, motorB)
-            log.info(
-                f"{key.number}:{key.value} OK servo={servo},motor={motorA},{motorB}")
+                continue
+        elif event.type == pygame.JOYBUTTONUP:
+            if event.button == 5:
+                speed += 10
+            if event.button == 4:
+                speed -= 10
 
-
-if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.INFO, format='%(asctime)s [%(name)s]%(levelname)s:%(message)s')
-    log = logging.getLogger("main")
-    devices = connect_to_board()
-    log.info(f"Found devices: {devices}")
-    camIP = devices['esp32-cam']
-    boardIP = devices['board']
-    run_event_loop(print_add, print_remove, key_received)
+    # print(f"lt = {lt}, rt={rt}")
+    servo = ((rt + 1) - (lt + 1)) * 5 / 2 + 7.5
+    servo = clamp(servo, 2.5, 12.5)
+    speed = clamp(speed, -100, 100)
+    print(f"Servo = {servo}")
+    motorA = 0 if speed < 0 else speed
+    motorB = 0 if speed > 0 else speed
+    control(servo=servo, motorA=motorA, motorB=motorB)
