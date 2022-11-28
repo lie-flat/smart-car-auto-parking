b846956d7049e3e5a9d9b44855dd45299a0e58c2
kxxt <rsworktech@outlook.com>
2022-11-28 17:24:31 +0800

    remove deprecated scripts

--- client/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This is a convenience script to start the client on raspberry pi
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-python "$SCRIPT_DIR"/run.py;python "$SCRIPT_DIR"/detect.py
