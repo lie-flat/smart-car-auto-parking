7d64d80c30dd2e2a1ffc4a6a32e5fb833672ec2e
kxxt <rsworktech@outlook.com>
2022-11-29 11:28:29 +0800

    update color of circle

--- client/run/cam.py
+++ client/run/cam.py
@@ -8,7 +8,7 @@ while True:
     ret, frame = vid.read()
     if not ret:
         raise Exception("Read failure!")
-    cv.circle(frame, (320, 240), 5, (255, 0, 0), 5)
+    cv.circle(frame, (320, 240), 3, (255, 255, 255), 3)
     cv.imshow('image', frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
