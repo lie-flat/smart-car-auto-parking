0a8be9e097e73d90761f8056c5364ff2f367e803
kxxt <rsworktech@outlook.com>
2022-12-15 12:32:48 +0800

    setup rl env : rollerball

--- environment/Assets/RollerAgent.cs
+++ environment/Assets/RollerAgent.cs
@@ -1,14 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 using UnityEngine;
 
-public class RollerAgent : MonoBehaviour
+public class RollerAgent : Agent
 {
+    Rigidbody rBody;
+    public Transform Target;
     // Start is called before the first frame update
-    void Start() { 
-        
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update() { }
+    public override void OnEpisodeBegin()
+    {
+        if (transform.localPosition.y < 0)
+        {
+            rBody.angularVelocity = Vector3.zero;
+            rBody.velocity = Vector3.zero;
+            transform.localPosition = new Vector3(0, 0.5f, 0);
+        }
+        Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(transform.localPosition);
+
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+    }
+    public float forceMultiplier = 10;
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actions.ContinuousActions[0];
+        controlSignal.z = actions.ContinuousActions[1];
+        rBody.AddForce(controlSignal * forceMultiplier);
+
+        // Rewards
+
+        float distanceToTarget = Vector3.Distance(transform.localPosition, Target.localPosition);
+
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+        else if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");
+    }
 }
