6671593fec6890d75c29d7e8c05649ae3e1b9e38
kxxt <rsworktech@outlook.com>
2022-12-23 16:57:41 +0800

    feat: sync action api  on realcar

--- client/controller/__init__.py
+++ client/controller/__init__.py
@@ -1,2 +1,2 @@
 from .communication import connect_to_board, read_sensors
-from .control import control, buzz
+from .control import control, buzz, act
--- client/controller/control.py
+++ client/controller/control.py
@@ -12,6 +12,16 @@ def control(ip, servo=None, motorA=None, motorB=None):
     requests.post(f"http://{ip}/cmd", body)
 
 
+def act(ip, servo=7.5, a=0, b=0, duration=0):
+    body = {
+        "servo": servo,
+        "motorA": a,
+        "motorB": b,
+        "duration": duration
+    }
+    requests.post(f"http://{ip}/act", body)
+
+
 def buzz(ip, freq=3000, duration=500):
     requests.post(f"http://{ip}/buzz", {
         'freq': freq, 'duration': duration
