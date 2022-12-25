9e21496df45a39d05011e95838d34c07c75c911e
kxxt <rsworktech@outlook.com>
2022-12-25 12:44:54 +0800

    register real env

--- client/rl/__init__.py
+++ client/rl/__init__.py
@@ -1,3 +1,4 @@
 from gym.envs.registration import register
 
 register(id='ParkingLot-v0', entry_point='client.rl.env:ParkingLotEnv')
+register(id='RealParkingLot-v0', entry_point='client.rl.real:RealParkingLotEnv')
